10b8ecb95f7fe627c6f1ff28c90a5f2f2dbab05b
natersland <natersland@gmail.com>
2024-05-31 21:23:32 +0700

    feat: impl time helper + test

--- common/common_helpers/uuid_helper.go
+++ common/common_helpers/uuid_helper.go
@@ -1,17 +1,10 @@
 package commonhelpers
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 )
 
 func GenerateUUID() string {
 	uuid := uuid.New()
 	return uuid.String()
-}
-
-func GetCurrentTimeISO() string {
-	currentTime := time.Now().UTC()         // Get the current UTC time
-	return currentTime.Format(time.RFC3339) // Format as ISO 8601
 }
